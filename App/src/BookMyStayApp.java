7f0d8dc3e10b86eecbb12fea9a5c7e58806de376
hanuman-star <kr8366@srmist.edu.in>
2026-04-07 14:57:17 +0530

    Use Case 1 added

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,23 @@
+public class BookMyStayApp {
+    /**
+     * Book My Stay App
+     * Use Case 1 - Application Entry & Welcome Message
+     *
+     * @author Student
+     * @version 1.0
+     */
+
+
+        public static void main(String[] args) {
+
+            System.out.println("=================================");
+            System.out.println("       BOOK MY STAY APP          ");
+            System.out.println("   Hotel Booking System v1.0     ");
+            System.out.println("=================================");
+
+            System.out.println("Welcome to Book My Stay Application");
+            System.out.println("Application Started Successfully");
+
+        }
+
+}
